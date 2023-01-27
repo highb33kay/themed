6f2935585463ed049ac85cd9c1c83bfe7e6fb29b
highb33kay <hiibeekayvibe@gmail.com>
2023-01-27 19:49:01 +0100

    title down

--- functions.php
+++ functions.php
@@ -3,7 +3,8 @@
 function themed_theme_support()
 {
     add_theme_support('title-tag');
-    add_theme_support( 'custom-logo' );
+    add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
 }
 
 add_action('after_setup_theme', 'themed_theme_support');
